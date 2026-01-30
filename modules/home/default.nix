38c866b8d685daa7ae5963984da362d829edd63e
meowveloper <meowveloper.dev@gmail.com>
2026-01-30 16:41:05 +0700

    tuis

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,6 +1,22 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     # ./nvf.nix
     ./nvim-wrapped.nix
+  ];
+
+  home.packages = with pkgs; [
+    # Terminal Tools (Available everywhere)
+    yazi
+    lazygit
+    ripgrep
+    fd
+    fzf
+    git
+    
+    # Development Basics
+    gcc
+    gnumake
+    nodejs
+    python3
   ];
 }
