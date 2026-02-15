39a376d0c3b489fe609d8ff5262718fbaff42fa3
meowveloper <meowveloper.dev@gmail.com>
2026-02-15 16:11:34 +0700

    waypaper

--- modules/home/desktop/meow/matugen.nix
+++ modules/home/desktop/meow/matugen.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }: {
     home.packages = with pkgs; [
         matugen
+        waypaper
+        swww
     ];
 
     xdg.configFile."matugen".source = ../../../../dot-files/meow/.config/matugen;
+    xdg.configFile."waypaper".source = ../../../../dot-files/meow/.config/waypaper;
 }
