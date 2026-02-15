937178cc6c0ce4c65b090834fb1076ee658483b9
meowveloper <meowveloper.dev@gmail.com>
2026-02-15 15:45:54 +0700

    waybar restart script

--- dot-files/meow/.config/waybar/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pkill -9 waybar
-
-waybar & disown
--- modules/home/desktop/meow/waybar.nix
+++ modules/home/desktop/meow/waybar.nix
@@ -6,4 +6,13 @@
     xdg.configFile."waybar/modules.jsonc".source = ../../../../dot-files/meow/.config/waybar/modules.jsonc;
     xdg.configFile."waybar/style.css".source = ../../../../dot-files/meow/.config/waybar/style.css;
 
+    xdg.configFile."waybar/restart.sh".text = ''
+        #!/usr/bin/env bash
+
+        pkill -9 waybar
+
+        waybar & disown
+    '';
+    xdg.configFile."waybar/restart.sh".executable = true;
+
 }
