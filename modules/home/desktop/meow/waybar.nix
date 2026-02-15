ec9d3474d0d469465517dd8a1975fe6c59e76578
meowveloper <meowveloper.dev@gmail.com>
2026-02-16 01:34:38 +0700

    waybar config dynamic

--- modules/home/desktop/meow/waybar.nix
+++ modules/home/desktop/meow/waybar.nix
@@ -1,13 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, config, userSettings, ... }: {
     services.network-manager-applet.enable = true;
     home.packages = with pkgs; [
         waybar
         networkmanagerapplet
         bluetui
     ];
-    xdg.configFile."waybar/config.jsonc".source = ../../../../dot-files/meow/.config/waybar/config.jsonc;
-    xdg.configFile."waybar/modules.jsonc".source = ../../../../dot-files/meow/.config/waybar/modules.jsonc;
-    xdg.configFile."waybar/style.css".source = ../../../../dot-files/meow/.config/waybar/style.css;
+
+    xdg.configFile."waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/meow/.config/waybar/config.jsonc";
+    xdg.configFile."waybar/modules.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/meow/.config/waybar/modules.jsonc";
+    xdg.configFile."waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/meow/.config/waybar/style.css";
 
     xdg.configFile."waybar/restart.sh".text = ''
         #!/usr/bin/env bash
