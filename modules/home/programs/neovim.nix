55b52d99a6d8640d54cdd74688ff328dc0b19f69
meowveloper <meowveloper.dev@gmail.com>
2026-02-24 19:25:14 +0700

    fix indent

--- modules/home/programs/neovim.nix
+++ modules/home/programs/neovim.nix
@@ -1,39 +1,39 @@
 { pkgs, config, userSettings, ... }: {
-  # Symlink your existing AstroNvim config
-  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/nvim";
+    # Symlink your existing AstroNvim config
+    xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/nvim";
 
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
+    programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
 
-    # These packages will be available in Neovim's PATH
-    extraPackages = with pkgs; [
-      # LSPs & Formatters
-      lua-language-server
-      stylua
-      typescript-language-server
-      vue-language-server
-      nodePackages.vscode-langservers-extracted # HTML, CSS, JSON, ESLint
-      tailwindcss-language-server
-      zls # Zig
-      clang-tools # C++, Clangd
-      dockerfile-language-server      
-      nixd
-      deadnix
-      statix
+        # These packages will be available in Neovim's PATH
+        extraPackages = with pkgs; [
+            # LSPs & Formatters
+            lua-language-server
+            stylua
+            typescript-language-server
+            vue-language-server
+            nodePackages.vscode-langservers-extracted # HTML, CSS, JSON, ESLint
+            tailwindcss-language-server
+            zls # Zig
+            clang-tools # C++, Clangd
+            dockerfile-language-server      
+            nixd
+            deadnix
+            statix
 
-      # Runtime / Compilers needed for plugins (Treesitter, etc.)
-      tree-sitter
-      unzip
-      wget
-      lua
-      luajitPackages.luarocks-nix
-      git
-      gcc
-      gnumake
-      curl
-    ];
-  };
+            # Runtime / Compilers needed for plugins (Treesitter, etc.)
+            tree-sitter
+            unzip
+            wget
+            lua
+            luajitPackages.luarocks-nix
+            git
+            gcc
+            gnumake
+            curl
+        ];
+    };
 }
