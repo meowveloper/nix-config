fc1661de00943d68fc1c8fc398d40892dffd1d9c
meowveloper <meowveloper.dev@gmail.com>
2026-02-24 19:22:09 +0700

    vim options

--- dot-files/programs/.config/nvim/lua/plugins/astrocore.lua
+++ dot-files/programs/.config/nvim/lua/plugins/astrocore.lua
@@ -6,30 +6,36 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
-        tabstop = 4,
-        shiftwidth = 4,
-        expandtab = true,
-      },
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+        -- vim options can be configured here
+        options = {
+            opt = { -- vim.opt.<key>
+                relativenumber = true, -- sets vim.opt.relativenumber
+                number = true, -- sets vim.opt.number
+                spell = false, -- sets vim.opt.spell
+                signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+                wrap = false, -- sets vim.opt.wrap
+                tabstop = 4,
+                shiftwidth = 4,
+                expandtab = true,
+                softtabstop = 4,
+                smartindent = true,
+                autoindent = true,
+                cursorline = true,
+                scrolloff = 20,
+                sidescrolloff = 8,
+            },
+        },
+        mappings = {
+            n = {
+                ["<Leader>bl"] = { function() require("telescope.builtin").buffers() end, desc = "List Buffers" },
+                ["<S-h>"] = { function() require("astrocore.buffer").nav(-1) end, desc = "Previous Buffer" },
+                ["<S-l>"] = { function() require("astrocore.buffer").nav(1) end, desc = "Next Buffer" },
+                ["[b"] = false,
+                ["]b"] = false,
+            },
+        },
     },
-    mappings = {
-      n = {
-        ["<Leader>bl"] = { function() require("telescope.builtin").buffers() end, desc = "List Buffers" },
-        ["<S-h>"] = { function() require("astrocore.buffer").nav(-1) end, desc = "Previous Buffer" },
-        ["<S-l>"] = { function() require("astrocore.buffer").nav(1) end, desc = "Next Buffer" },
-        ["[b"] = false,
-        ["]b"] = false,
-      },
-    },
-  },
 }
