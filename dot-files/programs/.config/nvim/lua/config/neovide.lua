07642e07edbb430558d12d2070f04691655e7479
meowveloper <meowveloper.dev@gmail.com>
2026-03-29 15:51:07 +0700

    fix paste issue in neovide

--- dot-files/programs/.config/nvim/lua/config/neovide.lua
+++ dot-files/programs/.config/nvim/lua/config/neovide.lua
@@ -19,4 +19,28 @@ if vim.g.neovide then
     vim.g.neovide_padding_right = 5
     vim.g.neovide_padding_left = 10
     vim.g.neovide_scale_factor = 1.0
+
+    -- 4. Keybindings for Copy/Paste
+    -- Helper function to map keys for multiple modes
+    local function map(mode, lhs, rhs, opts)
+        opts = opts or {}
+        opts.silent = opts.silent ~= false
+        vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- Paste from system clipboard
+    -- Normal and Visual modes
+    map({ "n", "v" }, "<C-v>", [["+p]])
+    map({ "n", "v" }, "<C-S-v>", [["+p]])
+
+    -- Insert and Command-line modes (using <C-r>+ for direct insertion)
+    map({ "i", "c" }, "<C-v>", [[<C-r>+]])
+    map({ "i", "c" }, "<C-S-v>", [[<C-r>+]])
+
+    -- Terminal mode
+    map("t", "<C-v>", [[<C-\><C-n>"+pi]])
+    map("t", "<C-S-v>", [[<C-\><C-n>"+pi]])
+    
+    -- Copy to system clipboard
+    map("v", "<C-c>", [["+y]])
 end
