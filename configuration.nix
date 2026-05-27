98629d1430008b701dcd1a1633cbda7afbd2e626
meowveloper <meowveloper.dev@gmail.com>
2026-05-27 21:14:47 +0700

    allow unprivileged user namespaces by default

--- configuration.nix
+++ configuration.nix
@@ -1,33 +1,36 @@
 { inputs, ... }:
 
 {
-  imports =
+    imports =
     [
-      ./hardware-configuration.nix
-      ./modules/system/users.nix
-      ./modules/system/boot.nix
-      ./modules/system/network.nix
-      ./modules/system/locale.nix
-      ./modules/system/display.nix
-      ./modules/system/mangowm.nix
-      ./modules/system/nix-settings.nix
-      ./modules/system/packages.nix
-      ./modules/system/shell.nix
-      ./modules/system/flatpak.nix
-      ./modules/system/audio.nix
-      ./modules/system/burmese.nix
-      ./modules/system/bluetooth.nix
-      ./modules/system/direnv.nix
-      ./modules/system/warp.nix
-      ./modules/system/fonts.nix
-      ./modules/system/battry-input.nix
-      ./modules/system/variables.nix
+        ./hardware-configuration.nix
+        ./modules/system/users.nix
+        ./modules/system/boot.nix
+        ./modules/system/network.nix
+        ./modules/system/locale.nix
+        ./modules/system/display.nix
+        ./modules/system/mangowm.nix
+        ./modules/system/nix-settings.nix
+        ./modules/system/packages.nix
+        ./modules/system/shell.nix
+        ./modules/system/flatpak.nix
+        ./modules/system/audio.nix
+        ./modules/system/burmese.nix
+        ./modules/system/bluetooth.nix
+        ./modules/system/direnv.nix
+        ./modules/system/warp.nix
+        ./modules/system/fonts.nix
+        ./modules/system/battry-input.nix
+        ./modules/system/variables.nix
     ];
 
-  system.stateVersion = "25.11";
-  fileSystems."/mnt/extra-volume" = {    
-    device = "/dev/disk/by-uuid/1ed14588-7c5b-4f65-98cc-0f3a746ea157";
-    fsType = "ext4";            
-    options = [ "defaults" "user" "exec" "nofail" ]; 
-  };
+    system.stateVersion = "25.11";
+    boot.kernel.sysctl = {
+        "kernel.unprivileged_userns_clone" = 1;
+    };
+    fileSystems."/mnt/extra-volume" = {
+        device = "/dev/disk/by-uuid/1ed14588-7c5b-4f65-98cc-0f3a746ea157";
+        fsType = "ext4";
+        options = [ "defaults" "user" "exec" "nofail" ];
+    };
 }
