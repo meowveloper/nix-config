5d01c78dec6e782038a91d705f09bff9d80256dc
meowveloper <meowveloper.dev@gmail.com>
2026-05-28 15:18:03 +0700

    user descriptions

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -1,15 +1,15 @@
 { userSettings, ... }:
 
 {
-  users.users.${userSettings.username1} = {
-    isNormalUser = true;
-    description = "Primary User";
-    extraGroups = [ "networkmanager" "wheel" "input" ];
-  };
+    users.users.${userSettings.username1} = {
+        isNormalUser = true;
+        description = "${userSettings.username1}";
+        extraGroups = [ "networkmanager" "wheel" "input" ];
+    };
 
-  users.users.${userSettings.username2} = {
-    isNormalUser = true;
-    description = "Secondary User";
-    extraGroups = [ "networkmanager" "wheel" "input" ];
-  };
+    users.users.${userSettings.username2} = {
+        isNormalUser = true;
+        description = "${userSettings.username2}";
+        extraGroups = [ "networkmanager" "wheel" "input" ];
+    };
 }
