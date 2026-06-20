a633963ef1c46a1bd4912accbd152e1b55f424fc
meowveloper <meowveloper.dev@gmail.com>
2026-06-20 15:06:43 +0700

    update (opencode-desktop-app, zed editor)

--- modules/home/programs/tuis.nix
+++ modules/home/programs/tuis.nix
@@ -6,12 +6,18 @@
         enableZshIntegration = true;
         shellWrapperName = "y";
     };
-    programs.opencode.enable = true;
+    home.packages = with pkgs;[
+        opencode
+        opencode-desktop
+        zed-editor-fhs
+    ];
 
     xdg.configFile."yazi/yazi.toml".source = ../../../dot-files/programs/.config/yazi/executable_yazi.toml;
     xdg.configFile."yazi/keymap.toml".source = ../../../dot-files/programs/.config/yazi/keymap.toml;
     xdg.configFile."yazi/launch.sh".source = ../../../dot-files/programs/.config/yazi/executable_launch.sh;
     xdg.configFile."fastfetch".source = ../../../dot-files/programs/.config/fastfetch;
+
+    xdg.dataFile."applications/opencode-desktop.desktop".source = "${pkgs.opencode-desktop}/share/applications/opencode-desktop.desktop";
 
     xdg.configFile."opencode/agents".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/opencode/agents";
     xdg.configFile."opencode/AGENTS.md".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/opencode/AGENTS.md";
