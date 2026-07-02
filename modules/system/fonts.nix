fb093d917928f356e045a9f197fd816018268c9f
meowveloper <meowveloper.dev@gmail.com>
2026-07-02 21:45:09 +0700

    fonts

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -2,9 +2,17 @@
     fonts.packages = with pkgs; [
         material-symbols
         noto-fonts-color-emoji
-        nerd-fonts.symbols-only 
+        nerd-fonts.symbols-only
         font-awesome
         fira-code
         inter-nerdfont
+        noto-fonts
     ];
+
+    fonts.fontconfig.defaultFonts = {
+        serif = [ "Noto Serif" ];
+        sansSerif = [ "Noto Sans" ];
+        monospace = [ "Fira Code" "Noto Sans Mono" ];
+        emoji = [ "Noto Color Emoji" ];
+    };
 }
