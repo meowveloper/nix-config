75b5fa5f06ece7c70ca3e307956a78be98f46771
meowveloper <meowveloper.dev@gmail.com>
2026-07-06 10:29:27 +0700

    update

--- modules/system/battry-input.nix
+++ modules/system/battry-input.nix
@@ -3,4 +3,8 @@
     services.upower.enable = true;
     services.power-profiles-daemon.enable = true;
     services.libinput.enable = true;
+
+    services.logind.lidSwitch = "lock";
+    services.logind.lidSwitchExternalPower = "lock";
+    services.logind.lidSwitchDocked = "ignore";
 }
