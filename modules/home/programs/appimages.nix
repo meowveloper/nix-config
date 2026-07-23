2fad373fede612bef5468844bc231c9d7479da32
meowveloper <meowveloper.dev@gmail.com>
2026-07-24 01:48:57 +0700

    myanso

--- modules/home/programs/appimages.nix
+++ modules/home/programs/appimages.nix
@@ -72,11 +72,48 @@
         cp -r ${browseros-desktop}/share/applications/* $out/share/applications/
         cp ${browseros-appimage-contents}/browseros.png $out/share/icons/hicolor/512x512/apps/browseros.png
     '';
+
+    # === Myanso ===
+    myanso-pname = "myanso";
+    myanso-version = "0.1.1";
+    myanso-src = pkgs.fetchurl {
+        url = "https://github.com/saturngod/myanso/releases/download/v0.1.1/Myanso-0.1.1.AppImage";
+        hash = "sha256-d6VqgQ6pyez6fxmMvfAnTkg9mVmWOVvv9vQ+QwmpmQo=";
+    };
+
+    myanso-appimage-contents = pkgs.appimageTools.extractType2 {
+        pname = myanso-pname;
+        version = myanso-version;
+        src = myanso-src;
+    };
+
+    myanso = pkgs.appimageTools.wrapType2 {
+        pname = myanso-pname;
+        version = myanso-version;
+        src = myanso-src;
+    };
+    myanso-desktop = pkgs.makeDesktopItem {
+        name = "myanso";
+        exec = "${myanso}/bin/myanso %U";
+        icon = "myanso";
+        comment = "Terminal emulator with Myanmar script support";
+        desktopName = "Myanso";
+        categories = [ "System" "TerminalEmulator" ];
+    };
+
+    myanso-integration = pkgs.runCommand "myanso-integration" {}''
+        mkdir -p $out/share/applications
+        mkdir -p $out/share/icons/hicolor/512x512/apps
+        cp -r ${myanso-desktop}/share/applications/* $out/share/applications/
+        cp ${myanso-appimage-contents}/.DirIcon $out/share/icons/hicolor/512x512/apps/myanso.png
+    '';
 in{
     home.packages = [
         esound
         esound-integration
         browseros
         browseros-integration
+        myanso
+        myanso-integration
     ];
 }
