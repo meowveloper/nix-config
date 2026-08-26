e3343e2c09cd633d85499b757c680899b022791b
meowveloper <meowveloper.dev@gmail.com>
2026-08-26 13:46:58 +0700

    add pi and cleanup

--- modules/home/programs/llm-agents.nix
+++ modules/home/programs/llm-agents.nix
@@ -1,20 +1,14 @@
 { pkgs, config, userSettings, inputs, ... }: {
     home.packages = [
         inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode
-        inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode2
+        inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.pi
     ];
-
 
     xdg.configFile."opencode/agents".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/opencode/agents";
     xdg.configFile."opencode/skills".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/opencode/skills";
     xdg.configFile."opencode/AGENTS.md".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/opencode/AGENTS.md";
     xdg.configFile."opencode/opencode.jsonc".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/opencode/opencode.jsonc";
     xdg.configFile."opencode/tui.json".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.config/opencode/tui.json";
-
-    home.file.".omp/agent/config.yml".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.omp/agent/config.yml";
-    home.file.".omp/agent/AGENTS.md".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.omp/agent/AGENTS.md";
-    home.file.".omp/agent/agents".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.omp/agent/agents";
-    home.file.".omp/agent/managed-skills".source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotfiles_path}/programs/.omp/agent/managed-skills";
 
     systemd.user.services.opencode-web = {
         Unit = {
